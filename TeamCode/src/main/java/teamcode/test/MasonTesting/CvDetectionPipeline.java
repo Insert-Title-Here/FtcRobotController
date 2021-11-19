d2fcf0b162a062f1813654d8493dcc52658874bd
ProDCG <mason.stuart05@outlook.com>
2021-11-19 15:12:36 -0800

    first run of the real time tracking contour system

--- TeamCode/src/main/java/teamcode/test/MasonTesting/CvDetectionPipeline.java
+++ TeamCode/src/main/java/teamcode/test/MasonTesting/CvDetectionPipeline.java
@@ -1,6 +1,7 @@
 package teamcode.test.MasonTesting;
 
 import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
 import org.opencv.core.Point;
@@ -14,48 +15,34 @@ import java.util.List;
 
 class CvDetectionPipeline extends OpenCvPipeline {
 
-    static final Scalar BLUE = new Scalar(0, 0, 255);
-    static final Scalar WHITE = new Scalar(255, 255, 255);
-
-    static final Point CREGION_TOPLEFT_ANCHOR_POINT = new Point(150, 110);
-    static final int REGION_WIDTH = 20;
-    static final int REGION_HEIGHT = 20;
-
-    Point cRegion_pointA = new Point(
-            CREGION_TOPLEFT_ANCHOR_POINT.x,
-            CREGION_TOPLEFT_ANCHOR_POINT.y);
-    Point cRegion_pointB = new Point(
-            CREGION_TOPLEFT_ANCHOR_POINT.x + REGION_WIDTH,
-            CREGION_TOPLEFT_ANCHOR_POINT.y + REGION_HEIGHT);
-
     List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
     Mat heirarchy = new Mat();
 
-    // TODO - research docs for following statements, and tweak values in regards to HSV and binary vals
-    // convert img to binary
-    public Mat srcToHSVBinary(Mat input) {
-        Mat hsvMat = new Mat(input.rows(), input.cols(), input.type());
-        Imgproc.cvtColor(input, hsvMat, Imgproc.COLOR_RGB2HSV);
-        Mat binary = new Mat(input.rows(), input.cols(), input.type(), new Scalar(0));
-        Imgproc.threshold(hsvMat, binary, 100, 255, Imgproc.THRESH_BINARY_INV);
-
-        return binary;
-    }
-
     // find contours
-    public void findImgContours(Mat binaryMat) {
-        Imgproc.findContours(binaryMat, contours, heirarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+    public void findImgContours(Mat frame) {
+        Mat labMat = new Mat(frame.rows(), frame.cols(), frame.type());
+        Imgproc.cvtColor(frame, labMat, Imgproc.COLOR_RGB2Lab);
+        Mat bMat = new Mat();
+        Core.extractChannel(labMat, bMat, 2);
+
+        Mat binary = new Mat(frame.rows(), frame.cols(), frame.type());
+        Imgproc.threshold(bMat, binary, 100, 255, Imgproc.THRESH_BINARY_INV);
+
+        // find contours
+        Imgproc.findContours(binary, contours, heirarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+
+        // draw contours
+        Mat draw = Mat.zeros(binary.size(), CvType.CV_8UC1);
+        for (int i = 0; i < contours.size(); i++) {
+            Scalar color = new Scalar(255, 255, 255);
+            Imgproc.drawContours(draw, contours, i, color, 2, Imgproc.LINE_8, heirarchy);
+        }
     }
 
     @Override
     public Mat processFrame(Mat input){
-        srcToHSVBinary(input);
         findImgContours(input);
 
         return input;
-    }
-
-    public String imgToString() {
-        return contours.toString();
     }
 }
