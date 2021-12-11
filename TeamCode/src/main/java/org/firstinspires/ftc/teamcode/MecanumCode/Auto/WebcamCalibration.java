2ca28060b62df69850a1c911c2b0ef7086947b39
Kevin <kcheck25@icloud.com>
2021-12-10 17:28:50 -0800

    code for mason

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumCode/Auto/WebcamCalibration.java
@@ -0,0 +1,55 @@
+package org.firstinspires.ftc.teamcode.MecanumCode.Auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.MecanumCode.Common.OpModeWrapper;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+
+import java.io.FileNotFoundException;
+
+@TeleOp(name = "Webcam Calibration")
+public class WebcamCalibration extends OpModeWrapper {
+
+    WebcamName webcam;
+    OpenCvCamera camera;
+    static final BarcodePipelineRed bcPipeline = new BarcodePipelineRed();
+
+    @Override
+    protected void onInitialize() throws FileNotFoundException {
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam = hardwareMap.get(WebcamName.class, "Webcam");
+
+        camera = OpenCvCameraFactory.getInstance().createWebcam(webcam, cameraMonitorViewId);
+        camera.setPipeline(bcPipeline);
+
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+
+            }
+
+            @Override
+            public void onError(int errorCode) {
+                telemetry.addData("Camera Init Error", errorCode);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        while (opModeIsActive()) {
+            telemetry.addData("left: ", bcPipeline.avg1);
+            telemetry.addData("middle: ", bcPipeline.avg2);
+            telemetry.addData("right: ", bcPipeline.avg3);
+            telemetry.addData("pos:", bcPipeline.getPos());
+            telemetry.update();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+
+    }
+}
