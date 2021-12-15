215ef54d75600f7558aa5f3ddef55814c025e11f
ProDCG <mason.stuart05@outlook.com>
2021-12-14 16:47:08 -0800

    According to all known laws of aviation,     there is no way a bee should be able to fly.     Its wings are too small to get its fat little body off the ground.     The bee, of course, flies anyway     because bees don't care what humans think is impossible.     Yellow, black. Yellow, black. Yellow, black. Yellow, black.     Ooh, black and yellow! Let's shake it up a little.     Barry! Breakfast is ready!     Ooming!     Hang on a second.     Hello?     - Barry? - Adam?     - Oan you believe this is happening? - I can't. I'll pick you up.     Looking sharp.     Use the stairs. Your father paid good money for those.     Sorry. I'm excited.     Here's the graduate. We're very proud of you, son.     A perfect report card, all B's.     Very proud.     Ma! I got a thing going here.     - You got lint on your fuzz. - Ow! That's me!     - Wave to us! We'll be in row 118,000. - Bye!     Barry, I told you, stop flying in the house!     - Hey, Adam. - Hey, Barry.     - Is that fuzz gel? - A little. Special day, graduation.     Never thought I'd make it.     Three days grade school, three days high school.     Those were awkward.     Three days college. I'm glad I took a day and hitchhiked around the hive.     You did come back different.     - Hi, Barry. - Artie, growing a mustache? Looks good.     - Hear about Frankie? - Yeah.     - You going to the funeral? - No, I'm not going.     Everybody knows, sting someone, you die.     Don't waste it on a squirrel. Such a hothead.     I guess he could have just gotten out of the way.     I love this incorporating an amusement park into our day.     That's why we don't need vacations.     Boy, quite a bit of pomp... under the circumstances.     - Well, Adam, today we are men. - We are!     - Bee-men. - Amen!     Hallelujah!     Students, faculty, distinguished bees,     please welcome Dean Buzzwell.     Welcome, New Hive Oity graduating class of...     ...9:15.     That concludes our ceremonies.     And begins your career at Honex Industries!     Will we pick ourjob today?     I heard it's just orientation.     Heads up! Here we go.     Keep your hands and antennas inside the tram at all times.     - Wonder what it'll be like? - A little scary.     Welcome to Honex, a division of Honesco     and a part of the Hexagon Group.     This is it!     Wow.     Wow.     We know that you, as a bee, have worked your whole life     to get to the point where you can work for your whole life.     Honey begins when our valiant Pollen Jocks bring the nectar to the hive.     Our top-secret formula     is automatically color-corrected, scent-adjusted and bubble-contoured     into this soothing sweet syrup     with its distinctive golden glow you know as...     Honey!     - That girl was hot. - She's my cousin!     - She is? - Yes, we're all cousins.     - Right. You're right. - At Honex, we constantly strive     to improve every aspect of bee existence.     These bees are stress-testing a new helmet technology.
    fucking init i stg

--- TeamCode/src/main/java/teamcode/test/MasonTesting/CvDetectionPipeline.java
+++ TeamCode/src/main/java/teamcode/test/MasonTesting/CvDetectionPipeline.java
@@ -26,6 +26,8 @@ import org.openftc.easyopencv.OpenCvPipeline;
 import java.util.ArrayList;
 import java.util.List;
 
+import teamcode.common.Debug;
+
 public class CvDetectionPipeline extends OpenCvPipeline {
 
     ArrayList<Double> xPList = new ArrayList<>();
@@ -36,109 +38,114 @@ public class CvDetectionPipeline extends OpenCvPipeline {
 
     final int focalLength = 32;
     final double cameraAngle = 68;
+    int iterator;
 
     @Override
     public void init(Mat frame) {
         // i hope this works lol
+        iterator = 0;
     }
 
     @Override
     public synchronized Mat processFrame(Mat frame){
-        // mat vars
-        Mat hsv = new Mat();
-        Mat blurred = new Mat();
-        Mat mask = new Mat();
-        Mat binary = new Mat();
-        ArrayList<MatOfPoint> contours = new ArrayList<>();
-
-        // change src mat to be frame param
-        Mat src = new Mat();
-        frame.copyTo(src);
-
-        // apply blur, change color, get range
-        Imgproc.GaussianBlur(src, blurred, new Size(7, 7), 0);
-        Imgproc.cvtColor(blurred, hsv, Imgproc.COLOR_BGR2HSV);
-        Core.inRange(hsv, lower_yellow, upper_yellow, mask);
-
-        // erosion and dilation noise reduction
-        Imgproc.erode(mask, mask, new Mat(), new Point(-1, -1), 2);
-        Imgproc.dilate(mask, mask, new Mat(), new Point(-1, -1), 2);
-
-        // get binary threshold
-        Imgproc.threshold(mask, binary, 100, 255, Imgproc.THRESH_BINARY_INV);
-
-        // get contours and create subdivided contours list
-        Imgproc.findContours(mask, contours, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
-        ArrayList<MatOfPoint> subContours = new ArrayList<>();
-
-        // get arc length to get the needed individual cubes
+//        // mat vars
+//        Mat hsv = new Mat();
+//        Mat blurred = new Mat();
+//        Mat mask = new Mat();
+//        Mat binary = new Mat();
+//        ArrayList<MatOfPoint> contours = new ArrayList<>();
+//
+//        // change src mat to be frame param
+//        Mat src = new Mat();
+//        frame.copyTo(src);
+//
+//        // apply blur, change color, get range
+//        Imgproc.GaussianBlur(src, blurred, new Size(7, 7), 0);
+//        Imgproc.cvtColor(blurred, hsv, Imgproc.COLOR_BGR2HSV);
+//        Core.inRange(hsv, lower_yellow, upper_yellow, mask);
+//
+//        // erosion and dilation noise reduction
+//        Imgproc.erode(mask, mask, new Mat(), new Point(-1, -1), 2);
+//        Imgproc.dilate(mask, mask, new Mat(), new Point(-1, -1), 2);
+//
+//        // get binary threshold
+//        Imgproc.threshold(mask, binary, 100, 255, Imgproc.THRESH_BINARY_INV);
+//
+//        // get contours and create subdivided contours list
+//        Imgproc.findContours(mask, contours, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+//        ArrayList<MatOfPoint> subContours = new ArrayList<>();
+//
+//        // get arc length to get the needed individual cubes
+////        for (int i = 0; i < contours.size(); i++) {
+////            double val = Imgproc.arcLength(new MatOfPoint2f(contours.get(i).toArray()), true);
+////            if (val > 25) {
+////                subContours.add(contours.get(i));
+////            }
+////        }
+//
+//        // get x and y vals + x and y cords
+//        xPList.clear();
+//        yPList.clear();
 //        for (int i = 0; i < contours.size(); i++) {
-//            double val = Imgproc.arcLength(new MatOfPoint2f(contours.get(i).toArray()), true);
-//            if (val > 25) {
-//                subContours.add(contours.get(i));
+//            ArrayList<Point> pointList = new ArrayList<>();
+//            Converters.Mat_to_vector_Point(contours.get(i), pointList);
+//            ArrayList<Integer> xList = new ArrayList<>();
+//            ArrayList<Integer> yList = new ArrayList<>();
+//
+//            for (Point p : pointList) {
+//                xList.add((int) p.x);
+//                yList.add((int) p.y);
 //            }
+//
+//            int xMin = Integer.MAX_VALUE;
+//            int xMax = Integer.MIN_VALUE;
+//            for (int num : xList) {
+//                if (num < xMin) {
+//                    xMin = num;
+//                }
+//
+//                if (num > xMax) {
+//                    xMax = num;
+//                }
+//            }
+//
+//            int yMin = Integer.MAX_VALUE;
+//            int yMax = Integer.MIN_VALUE;
+//            for (int num : yList) {
+//                if (num < yMin) {
+//                    yMin = num;
+//                }
+//
+//                if (num > yMax) {
+//                    yMax = num;
+//                }
+//            }
+//
+//            int xCord = xMin + Math.abs((xMax - xMin) / 2);
+//            int yCord = yMin + Math.abs((yMax - yMin) / 2);
+//            int objWidth = Math.abs(xMax - xMin);
+//            int objHeight = Math.abs(yMax - xMin);
+//            Imgproc.drawMarker(src, new Point(xCord, yCord), new Scalar(255, 255, 255));
+//
+//            int srcWidth = src.width();
+//            int srcHeight = src.height();
+//
+//            // get distance
+//            double distance = distanceFinder(50.8, objWidth);
+//
+//            // flip around x/y coordinates due to how the robot uses vector movement
+//            double yPos = xCord; double xAmt = 0;
+//            xAmt = distance * Math.sin(cameraAngle);
+//            xAmt = Math.round((xAmt * 100.0) / 100.0);
+//            xPList.add(xAmt);
+//            yPList.add(yPos);
 //        }
-
-        // get x and y vals + x and y cords
-        xPList.clear();
-        yPList.clear();
-        for (int i = 0; i < contours.size(); i++) {
-            ArrayList<Point> pointList = new ArrayList<>();
-            Converters.Mat_to_vector_Point(contours.get(i), pointList);
-            ArrayList<Integer> xList = new ArrayList<>();
-            ArrayList<Integer> yList = new ArrayList<>();
-
-            for (Point p : pointList) {
-                xList.add((int) p.x);
-                yList.add((int) p.y);
-            }
-
-            int xMin = Integer.MAX_VALUE;
-            int xMax = Integer.MIN_VALUE;
-            for (int num : xList) {
-                if (num < xMin) {
-                    xMin = num;
-                }
-
-                if (num > xMax) {
-                    xMax = num;
-                }
-            }
-
-            int yMin = Integer.MAX_VALUE;
-            int yMax = Integer.MIN_VALUE;
-            for (int num : yList) {
-                if (num < yMin) {
-                    yMin = num;
-                }
-
-                if (num > yMax) {
-                    yMax = num;
-                }
-            }
-
-            int xCord = xMin + Math.abs((xMax - xMin) / 2);
-            int yCord = yMin + Math.abs((yMax - yMin) / 2);
-            int objWidth = Math.abs(xMax - xMin);
-            int objHeight = Math.abs(yMax - xMin);
-            Imgproc.drawMarker(src, new Point(xCord, yCord), new Scalar(255, 255, 255));
-
-            int srcWidth = src.width();
-            int srcHeight = src.height();
-
-            // get distance
-            double distance = distanceFinder(50.8, objWidth);
-
-            // flip around x/y coordinates due to how the robot uses vector movement
-            double yPos = xCord; double xAmt = 0;
-            xAmt = distance * Math.sin(cameraAngle);
-            xAmt = Math.round((xAmt * 100.0) / 100.0);
-            xPList.add(xAmt);
-            yPList.add(yPos);
-        }
-        Imgproc.line(src, new Point(src.width() / 2, src.height()), new Point(src.width() / 2, 0), new Scalar(255, 255, 255));
-        Imgproc.drawContours(src, contours, -1, new Scalar(255, 255, 255), 2, Imgproc.LINE_8);
-        return src;
+//        Imgproc.line(src, new Point(src.width() / 2, src.height()), new Point(src.width() / 2, 0), new Scalar(255, 255, 255));
+//        Imgproc.drawContours(src, contours, -1, new Scalar(255, 255, 255), 2, Imgproc.LINE_8);
+        iterator++;
+        Debug.log(iterator);
+        // comment part means src to frame, change back to src when comment heap is done
+        return frame;
     }
 
     public double distanceFinder(double realWidth, double pixelWidth) {
