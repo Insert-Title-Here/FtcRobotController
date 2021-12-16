724d226f48d7f7727ff8cd39da29c1368f0c5d4e
MrJamesGaming <52509738+MrJamesGaming@users.noreply.github.com>
2021-12-16 13:30:55 -0800

    Moskau Moskau
    Moskau
    Dschinghis Khan
    Moskau
    Fremd und geheimnisvoll
    Türme aus rotem Gold
    Kalt wie das Eis
    Moskau
    Doch wer dich wirklich kennt
    Der weiß, ein Feuer brennt
    In dir so heiß
    Kosaken hey hey hey hebt die Gläser (hey, hey)
    Natascha ha ha ha du bist schön (ha ha)
    Towarisch hey hey hey auf das Leben (hey, hey)
    Auf dein Wohl Bruder hey Bruder (hey, hey, hey, hey)
    Moskau, Moskau
    Wirf die Gläser an die Wand
    Russland ist ein schönes Land
    Ho ho ho ho ho, hey
    Moskau, Moskau
    Deine Seele ist so groß
    Nachts da ist der Teufel los
    Ha ha ha ha ha, hey
    Moskau, Moskau
    Liebe schmeckt wie Kaviar
    Mädchen sind zum küssen da
    Ho ho ho ho ho, hey
    Moskau, Moskau
    Komm wir tanzen auf dem Tisch
    Bis der Tisch zusammenbricht
    Ha ha ha ha ha
    Moskau
    Tor zur Vergangenheit
    Spiegel der Zarenzeit
    Rot wie das Blut
    Moskau
    Wer deine Seele kennt
    Der weiß, die Liebe brennt
    Heiß wie die Glut
    Kosaken hey hey hey hebt die Gläser (hey, hey)
    Natascha ha ha ha du bist schön (ha ha)
    Towarisch hey hey hey auf die Liebe (hey, hey)
    Auf dein Wohl Mädchen hey Mädchen ho (hey, hey, hey, hey)
    Moskau, Moskau
    Wirf die Gläser an die Wand
    Russland ist ein schönes Land
    Ho ho ho ho ho, hey
    Moskau, Moskau
    Deine Seele ist so groß
    Nachts da ist der Teufel los
    Ha ha ha ha ha, hey
    Moskau
    Lala lala lala la, lala lala lala la
    Ho ho ho ho ho, hey
    Moskau (Moskau)
    Lala lala lala la, lala lala lala la
    Ha ha ha ha ha
    Oh oh, oh oh, oh, oh
    Oh oh oh oh, oh, oh, oh (Moskau)
    (Moskau) (hey hey, hey, hey, hey, hey)
    Moskau, Moskau
    Liebe schmeckt wie Kaviar
    Mädchen sind zum küssen da
    Ho ho ho ho ho
    Moskau, Moskau
    Komm wir tanzen auf dem Tisch
    Bis der Tisch zusammenbricht
    Ha ha ha ha ha, hey

--- TeamCode/src/main/java/teamcode/Competition/Subsystems/ArmSystem.java
+++ TeamCode/src/main/java/teamcode/Competition/Subsystems/ArmSystem.java
@@ -20,9 +20,9 @@ public class ArmSystem {
     private static final double HOUSING_POSITION = 0.37; //these values are great, the scoring one MAYBE move up a lil but no more than 0.66 because it grinds at that point
     private static final double SCORING_POSITION = 0.66;
 
-    private static final double LINKAGE_DOWN = 0.0; //these values need to be refined but they are good ballparks. AYUSH: No longer a final constant.
-    private static final double LINKAGE_HOUSED = 0.6;
-    private static final double LINKAGE_SCORE = 0.7;
+    private static final double LINKAGE_DOWN = 0.4; //these values need to be refined but they are good ballparks. AYUSH: No longer a final constant.
+    private static final double LINKAGE_HOUSED = 0.7;
+    private static final double LINKAGE_SCORE = 0.8;
 
 
     private static final float GREEN_THRESHOLD = 255; //not needed for now
@@ -199,7 +199,7 @@ public class ArmSystem {
     }
 
     public void retract(){
-        moveSlide(-SLIDE_POWER, 100);
+        moveSlide(-0.5, 100);
        idleServos();
     }
 
