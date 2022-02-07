c19ebba7e5014ad3651baa99ad85b2a1aabd7bae
Insert-Title-Here <krishpdoshi@gmail.com>
2022-02-06 16:15:57 -0800

    New Chassis Code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewChassis/Common/NewConstants.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.NewChassis.Common;
+
+public class NewConstants {
+
+    //TODO: Change the Constants
+
+    // Game Constants
+    public static final int TOP_GOAL_POS = -2650;
+    public static final int MID_GOAL_POS = -1486;
+    public static final int BOTTOM_GOAL_POS = -300;
+    public static final int CAPPING_POS = -4100;
+    public static final int MAX_MANUAL_CAP = -4200;
+
+    public static final int MAGARM_EXTENDED = -275;
+    public static final int MAGARM_RETRACTED = -100;
+
+    public static final double LEVEL_DOWN_POS = 0;
+    public static final double LEVEL_UP_POS = 1;
+    public static final int TICS_TO_FOOT = 2000;
+}
