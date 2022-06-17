2dd18214073c041cbe3e45934d95a3cc36a0e3ae
Insert-Title-Here <krishpdoshi@gmail.com>
2022-06-16 22:28:36 -0700

    wanted to test out separate class 4 intake and made separate thread

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class Intake {
+    /*public enum ServoPosition{
+        Clamped,
+        Open
+    }
+
+     */
+
+    Servo lJoint, rJoint;
+    CRServo lIntake, rIntake;
+
+    public Intake(HardwareMap hardwareMap){
+        lJoint = hardwareMap.get(Servo.class, "lJoint");
+        rJoint = hardwareMap.get(Servo.class, "rJoint");
+        lIntake = hardwareMap.get(CRServo.class, "lIntake");
+        rIntake = hardwareMap.get(CRServo.class, "rIntake");
+
+        lJoint.setPosition(1);
+        rJoint.setPosition(0);
+
+    }
+
+    public void clampAndRelease(boolean toClamp){
+        if (toClamp) {
+            lJoint.setPosition(0.9);
+            rJoint.setPosition(0.1);
+        } else {
+            lJoint.setPosition(1);
+            rJoint.setPosition(0);
+        }
+    }
+
+    public void setPower(boolean rT, double power){
+        if (rT) {
+            lIntake.setPower(-power);
+            rIntake.setPower(power);
+        } else{
+            lIntake.setPower(power);
+            rIntake.setPower(-power);
+        }
+    }
+}
