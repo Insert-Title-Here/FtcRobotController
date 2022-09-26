43c3582afdfdc8408d70f40679bcf5520947fb53
KCheck25 <kcheck25@icloud.com>
2022-09-26 16:32:07 -0700

    Added basic mecanum drive code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TestTeleOp.java
@@ -0,0 +1,55 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.Common.MecanumDrive;
+import org.firstinspires.ftc.teamcode.Common.Vector2D;
+
+@TeleOp
+public class TestTeleOp extends LinearOpMode {
+
+    MecanumDrive drive;
+
+
+    private final double NORMAL_LINEAR_MODIFIER = 0.6;
+    private final double NORMAL_ROTATIONAL_MODIFIER = 0.6;
+    private final double SPRINT_LINEAR_MODIFIER = 1;
+    private final double SPRINT_ROTATIONAL_MODIFIER = 1;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        drive = new MecanumDrive(hardwareMap, telemetry);
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+
+
+        while(opModeIsActive()){
+
+
+            //TODO: Decide if you want sprint capability
+            if (gamepad1.right_bumper) { // replace this with a button for sprint
+                drive.setPower(new Vector2D(gamepad1.left_stick_x * SPRINT_LINEAR_MODIFIER, gamepad1.left_stick_y * SPRINT_LINEAR_MODIFIER), gamepad1.right_stick_x * SPRINT_ROTATIONAL_MODIFIER, false);
+            }
+            else {
+                drive.setPower(new Vector2D(gamepad1.left_stick_x * NORMAL_LINEAR_MODIFIER, gamepad1.left_stick_y * NORMAL_LINEAR_MODIFIER), gamepad1.right_stick_x * NORMAL_ROTATIONAL_MODIFIER, false);
+            }
+
+
+
+            telemetry.addData("flPos", drive.getFLPosition());
+            telemetry.addData("frPos", drive.getFRPosition());
+            telemetry.addData("blPos", drive.getBLPosition());
+            telemetry.addData("brPos", drive.getBRPosition());
+            telemetry.update();
+
+
+        }
+
+        drive.setPower(0, 0, 0, 0);
+    }
+}
