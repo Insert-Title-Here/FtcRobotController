9eae2a524f13b350e54366199dfab6efe2ae758b
InsertNameHere-Krish <krish.doshi@digipen.edu>
2022-09-26 08:36:26 -0700

    Finish CapMechTest

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/League1/Testing/CapMechTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/League1/Testing/CapMechTest.java
@@ -1,4 +1,39 @@
 package org.firstinspires.ftc.teamcode.League1.Testing;
 
-public class CapMechTest {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+
+import org.firstinspires.ftc.teamcode.League1.Common.Constants;
+import org.firstinspires.ftc.teamcode.League1.Common.Robot;
+import org.firstinspires.ftc.teamcode.League1.Common.Vector2D;
+import org.firstinspires.ftc.teamcode.League1.Subsystems.MecDrive;
+import org.firstinspires.ftc.teamcode.League1.Subsystems.ScoringSystem;
+
+
+//TODO: figure out bulk read
+
+@TeleOp
+public class CapMechTest extends LinearOpMode {
+
+    CRServo servo;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        servo = hardwareMap.get(CRServo.class, "CapExtend");
+
+        waitForStart();
+
+        while(opModeIsActive()){
+
+            if (gamepad1.right_trigger > 0.1) {
+                servo.setPower(1);
+            } else if(gamepad1.left_trigger > 0.1) {
+                servo.setPower(-1);
+            }else{
+                servo.setPower(0);
+            }
+        }
+
+        servo.setPower(0);
+    }
 }
