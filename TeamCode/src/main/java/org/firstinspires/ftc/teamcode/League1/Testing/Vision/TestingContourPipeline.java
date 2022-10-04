28d1f3aa6e95be2216fc4172e9f8560a872083f9
Kevin <kcheck25@icloud.com>
2022-10-03 18:49:39 -0700

    random vision stuff
    (Also first commit to talons branch lol)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/League1/Testing/Vision/TestingContourPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/League1/Testing/Vision/TestingContourPipeline.java
@@ -9,6 +9,8 @@ import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.imgproc.Moments;
+import org.opencv.utils.Converters;
 import org.openftc.easyopencv.OpenCvPipeline;
 
 import java.util.ArrayList;
@@ -18,6 +20,7 @@ public class TestingContourPipeline extends OpenCvPipeline {
 
     Telemetry telemetry;
     Mat temp = new Mat();
+    private ArrayList<Integer> xList, yList;
 
 
 
@@ -34,7 +37,7 @@ public class TestingContourPipeline extends OpenCvPipeline {
         //Yellow
         Core.inRange(temp, new Scalar(25, 180, 50), new Scalar(27, 255, 255), temp);
 
-        Imgproc.GaussianBlur(temp, temp, new Size(3, 3), 0);
+        Imgproc.GaussianBlur(temp, temp, new Size(5, 5), 0);
 
 
 
@@ -47,10 +50,26 @@ public class TestingContourPipeline extends OpenCvPipeline {
         //Find out last two things
         Imgproc.findContours(temp, contours, new Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
 
+        ArrayList<Point> pointList = new ArrayList<>();
+
+        xList = new ArrayList<>();
+        yList = new ArrayList<>();
+
+        double pointX;
+        double pointY;
+
 
         for(int i = 0; i < contours.size(); i++){
             telemetry.addData("contour" + i + ": ", contours.get(i).toArray().length);
         }
+
+        Moments moments = Imgproc.moments(temp, true);
+
+        Converters.Mat_to_vector_Point(contours.get(0), pointList);
+
+        telemetry.addData("Countours: ", contours.size());
+        telemetry.addData("Points: ", pointList.size());
+
 
         telemetry.update();
 
