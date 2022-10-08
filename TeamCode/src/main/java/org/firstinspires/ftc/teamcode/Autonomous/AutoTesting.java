df044b850921ae194974f45c8fb2be3db313f733
Nikhil Venkat <64997334+Nike-Hill@users.noreply.github.com>
2022-10-07 17:29:57 -0700

    Stopped slides from slipping when LT isn't pressed.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutoTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutoTesting.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.Autonomous;
 
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -8,12 +9,13 @@ import com.qualcomm.robotcore.hardware.Servo;
 import org.firstinspires.ftc.teamcode.Common.MecanumDrive;
 import org.firstinspires.ftc.teamcode.Common.Vector2D;
 
+@Autonomous
 public class AutoTesting extends LinearOpMode {
 
     MecanumDrive drive;
 
     //TODO: Move scoring system stuff to its own class
-    DcMotor lift;
+    DcMotor fr, fl, br, bl, lift;
     Servo clawl;
     Servo clawr;
 
@@ -27,6 +29,11 @@ public class AutoTesting extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
 
         drive = new MecanumDrive(hardwareMap, telemetry);
+        fr = hardwareMap.get(DcMotor.class, "fr");
+        fl = hardwareMap.get(DcMotor.class, "fl");
+        br = hardwareMap.get(DcMotor.class, "br");
+        bl = hardwareMap.get(DcMotor.class, "bl");
+
         lift = hardwareMap.get(DcMotor.class, "lift");
         clawl = hardwareMap.get(Servo.class, "clawl");
         clawr = hardwareMap.get(Servo.class, "clawr");
@@ -43,6 +50,7 @@ public class AutoTesting extends LinearOpMode {
         //lift.setPower();
         lift.setPower(0);
 
+
         for(int i = 0; i < 20; i++) {
             clawl.setPosition(0);
             clawr.setPosition(0.6);
@@ -55,11 +63,11 @@ public class AutoTesting extends LinearOpMode {
 
 
 
-            telemetry.addData("flPos", drive.getFLPosition());
-            telemetry.addData("frPos", drive.getFRPosition());
-            telemetry.addData("blPos", drive.getBLPosition());
-            telemetry.addData("brPos", drive.getBRPosition());
-            telemetry.update();
+        telemetry.addData("flPos", drive.getFLPosition());
+        telemetry.addData("frPos", drive.getFRPosition());
+        telemetry.addData("blPos", drive.getBLPosition());
+        telemetry.addData("brPos", drive.getBRPosition());
+        telemetry.update();
 
 
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TestTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TestTeleOp.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.Common.MecanumDrive;
 import org.firstinspires.ftc.teamcode.Common.Vector2D;
 
@@ -24,6 +25,12 @@ public class TestTeleOp extends LinearOpMode {
     private final double SPRINT_LINEAR_MODIFIER = 1;
     private final double SPRINT_ROTATIONAL_MODIFIER = 1;
 
+
+    boolean liftIsUp = false;
+    int origLiftPos;
+    int liftPos;
+    boolean clawIsClosed = true;
+
     @Override
     public void runOpMode() throws InterruptedException {
 
@@ -31,6 +38,10 @@ public class TestTeleOp extends LinearOpMode {
         lift = hardwareMap.get(DcMotor.class, "lift");
         clawl = hardwareMap.get(Servo.class, "clawl");
         clawr = hardwareMap.get(Servo.class, "clawr");
+
+        int origliftPos = lift.getCurrentPosition();
+        int liftPos = lift.getCurrentPosition();
+
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -44,29 +55,48 @@ public class TestTeleOp extends LinearOpMode {
         while(opModeIsActive()){
 
             if (gamepad1.right_bumper) {
+                liftIsUp = false;
                 drive.setPower(new Vector2D(gamepad1.right_stick_x * SPRINT_LINEAR_MODIFIER, gamepad1.left_stick_y * SPRINT_LINEAR_MODIFIER), gamepad1.left_stick_x * SPRINT_ROTATIONAL_MODIFIER, false);
+
             }
             else {
                 drive.setPower(new Vector2D(gamepad1.right_stick_x * NORMAL_LINEAR_MODIFIER, gamepad1.left_stick_y * NORMAL_LINEAR_MODIFIER), gamepad1.left_stick_x * NORMAL_ROTATIONAL_MODIFIER, false);
             }
 
             if (gamepad1.left_trigger > 0.1) {
-                lift.setPower(gamepad1.left_trigger / 2);
+                lift.setPower(gamepad1.left_trigger*0.8);
+                liftPos = lift.getCurrentPosition();
+                liftIsUp = true;
             } else if (gamepad1.right_trigger > 0.1) {
-                lift.setPower(-gamepad1.right_trigger / 2);
+                lift.setPower(-gamepad1.right_trigger*0.5);
+                liftPos = lift.getCurrentPosition();
+                liftIsUp = true;
             } else {
+                if(liftIsUp){
+                    while(opModeIsActive()&&lift.getCurrentPosition()<(liftPos-50)) {
+                        lift.setPower(0.1);
+                        telemetry.addData("Looping", "Adjusting lift position");
+                    }
+                }
                 lift.setPower(0);
             }
 
-            if(gamepad1.left_bumper){
-                clawl.setPosition(0);
-                clawr.setPosition(0.6);
+            if(lift.getCurrentPosition()==origLiftPos){
+                liftIsUp=false;
             }
 
-            if(gamepad1.right_bumper){
-                clawr.setPosition(0);
-                clawl.setPosition(0.6);
+            if(gamepad1.left_bumper){
+                if(clawIsClosed){
+                    clawr.setPosition(0);
+                    clawl.setPosition(0.6);
+                } else{
+                    clawl.setPosition(0);
+                    clawr.setPosition(0.6);
+                }
+
+                clawIsClosed = !clawIsClosed;
             }
+
 
 
 
