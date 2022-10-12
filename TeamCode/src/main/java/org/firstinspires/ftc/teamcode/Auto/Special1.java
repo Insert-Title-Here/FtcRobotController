d744813df676fe7d43af0f9c94a43f64501506a4
LegendsNeverDl3 <1042154@apps.nsd.org>
2022-10-11 17:21:55 -0700

    Added New classes to auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/Special1.java
@@ -0,0 +1,84 @@
+package org.firstinspires.ftc.teamcode.Auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.Common.MecanumDrive;
+import org.firstinspires.ftc.teamcode.Common.ScoringSystem;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+@Autonomous
+public class Special1 extends LinearOpMode {
+    MecanumDrive drive;
+    ScoringSystem score;
+    //OpenCvWebcam webcam;
+    AtomicBoolean cont;
+    Thread liftThread;
+    int parkLocation;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        drive = new MecanumDrive(hardwareMap, telemetry);
+        score = new ScoringSystem(hardwareMap);
+        cont = new AtomicBoolean();
+        cont.set(false);
+        /*
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "webcam"), cameraMonitorViewId);
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+         */
+
+        // Camera checks sleeve...stores parking location??
+
+        //TODO: Possibly change turns from encoder to IMU angles
+        //TODO: Work on auto for all the side (make different methods for each side?)
+
+        liftThread = new Thread(){
+            @Override
+            public void run(){
+                while(cont.get()){
+                    score.setPower(0.1);
+                }
+
+
+            }
+        };
+
+
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        waitForStart();
+
+        liftThread.start();
+
+    }
+    public int avgPosition(int fl, int fr, int bl, int br){
+        return (int)(Math.abs(fl) + Math.abs(fr) + Math.abs(bl) + Math.abs(br))/4;
+    }
+    /*
+    //TODO: check if camera angle works
+    private class DetectionAlgorithm extends OpenCvPipeline {
+        Mat original;
+        Mat changed;
+
+        @Override
+        public Mat processFrame(Mat input) {
+            input.copyTo(original);
+            changed = new Mat();
+            if(original.empty()) {
+                return input;
+            }
+            // cyan magenta yellow
+            Imgproc.cvtColor(original, changed, Imgproc.COLOR_RGB2YCrCb);
+            // magenta 255, 0, 255
+            Core.inRange(changed, new Scalar(240, 0 ,240), new Scalar(255, 0, 255), changed);
+            return null;
+        }
+
+
+    }
+    */
+
+}
