5bcec4754d0bfe8b1bff0a00de767c38233fd57f
KCheck25 <kcheck25@icloud.com>
2022-10-15 12:35:58 -0700

    FTCDashboard Vision Test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PowerPlay/Constants.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.PowerPlay;
+
+import com.acmerobotics.dashboard.config.Config;
+
+@Config
+public class Constants {
+    public static double CAMERA_POS = 0;
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PowerPlay/ServoTest.java
@@ -0,0 +1,64 @@
+package org.firstinspires.ftc.teamcode.PowerPlay;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvWebcam;
+
+
+@Autonomous
+public class ServoTest extends LinearOpMode {
+
+    OpenCvWebcam camera;
+    SignalPipeline pipeline;
+
+    Servo cameraServo;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        cameraServo = hardwareMap.get(Servo.class, "cameraServo");
+
+        WebcamName webcamName = hardwareMap.get(WebcamName.class, "Webcam 1");
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        camera = OpenCvCameraFactory.getInstance().createWebcam(webcamName, cameraMonitorViewId);
+        pipeline = new SignalPipeline(telemetry);
+
+        camera.setPipeline(pipeline);
+
+        telemetry.addData("Servo pos", cameraServo.getPosition());
+
+        //cameraServo.setPosition(Constants.CAMERA_POS);
+
+
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                camera.startStreaming(320, 176, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+                telemetry.addData("Camera Init Error", errorCode);
+                telemetry.update();
+
+            }
+        });
+
+        waitForStart();
+
+        //SignalPipeline.ParkPos position = pipeline.getPosition();
+
+        while(opModeIsActive()){
+
+        }
+
+        camera.closeCameraDevice();
+    }
+}
