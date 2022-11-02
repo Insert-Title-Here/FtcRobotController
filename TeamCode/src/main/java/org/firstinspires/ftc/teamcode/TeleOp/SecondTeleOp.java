9151290e4b2735b16c6fe742473d9d0630392961
Experance <100961120+Experance@users.noreply.github.com>
2022-11-01 17:47:03 -0700

    Update SecondTeleOp.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/SecondTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/SecondTeleOp.java
@@ -5,29 +5,25 @@ import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.teamcode.Common.ColorSensor;
 import org.firstinspires.ftc.teamcode.Common.MecanumDrive;
 import org.firstinspires.ftc.teamcode.Common.ScoringSystem;
 import org.firstinspires.ftc.teamcode.Common.Vector2D;
 
 import java.util.concurrent.atomic.AtomicBoolean;
-
 @TeleOp
 public class SecondTeleOp extends LinearOpMode {
-
     //TODO: change names if you want to
     MecanumDrive drive;
     ScoringSystem score;
     Thread liftThread;
     AtomicBoolean pause;
-    AtomicBoolean discontinue;
     BNO055IMU imu;
-    ColorSensor color;
+    AtomicBoolean discontinue;
+    //ColorSensor color;
     private final double NORMAL_LINEAR_MODIFIER = 0.7;
     private final double NORMAL_ROTATIONAL_MODIFIER = 0.7;
     private final double SPRINT_LINEAR_MODIFIER = 1;
     private final double SPRINT_ROTATIONAL_MODIFIER = 1;
-
     @Override
     public void runOpMode() throws InterruptedException {
         double initialAngle;
@@ -37,7 +33,6 @@ public class SecondTeleOp extends LinearOpMode {
         parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // seehe calibration sample opmode
         parameters.loggingEnabled = true;
         parameters.loggingTag = "IMU";
-
         parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
         imu = hardwareMap.get(BNO055IMU.class, "imu");
         imu.initialize(parameters);
@@ -45,31 +40,27 @@ public class SecondTeleOp extends LinearOpMode {
         score = new ScoringSystem(hardwareMap);
         pause = new AtomicBoolean();
         discontinue = new AtomicBoolean();
-        color = new ColorSensor(hardwareMap, telemetry);
+        //color = new ColorSensor(hardwareMap, telemetry);
         pause.set(true);
         discontinue.set(false);
         telemetry.addData("Status", "Initialized");
         telemetry.update();
-
-
         //score = new ScoringSystem(hardwareMap);
-
         //Open
         score.setClawPosition(0);
-        score.resetLiftEncoder();
         //TODO: Test below Out
         //Comment out code in opmodeisactive while loop if you test this tread out(as well as the thread aboeve)
-
         liftThread = new Thread(){
             @Override
             public void run(){
                 //removed !score.isBusy() from the while statement
                 while(opModeIsActive()){
                     if(gamepad1.right_bumper){
+                        score.setPower(0.6);
                         if(score.getEncoderPosition() > 2390){
                             score.setPower(0.1);
                         }else{
-                            score.setPower(0.6);
+                            score.setPower(1);
                         }
                     }else if(gamepad1.left_bumper) {
                         if(score.getEncoderPosition() < 2){
@@ -84,13 +75,9 @@ public class SecondTeleOp extends LinearOpMode {
                     // low cone, 13 in, 1209  gamenpad1.dpad_left
                     // medium cone, 23 in, 1795 gamepad1.dpad_up
                     // high cone, 33 in, 2390 gamepad1.dpad_right
-
                     if(gamepad1.dpad_down) {
                         score.goToPosition(0, 0.4);
                     }
-
-
-
                     if (gamepad1.right_stick_button) {
                         //high cone
                         score.goToPosition(2330, 0.95);
@@ -102,21 +89,19 @@ public class SecondTeleOp extends LinearOpMode {
                         score.setPower(0.1);
                     }
                     if(gamepad1.right_trigger > 0.1 && pause.get()){
-
                         if(0.22 < score.getClawPosition() && score.getClawPosition() < 0.26){
                             if(score.getEncoderPosition() > 1500){
                                 score.goToPosition(score.getEncoderPosition() - 300, 0.5);
                             }
                             score.setClawPosition(0);
+
                             try {
                                 Thread.sleep(800);
                             } catch (InterruptedException e) {
                                 e.printStackTrace();
                             }
                             score.goToPosition(0, 0.4);
-
                         }else{
-
                             if(score.getEncoderPosition() < 200){
                                 if(discontinue.get()){
                                     score.setClawPosition(0.25);
@@ -136,7 +121,6 @@ public class SecondTeleOp extends LinearOpMode {
                                     }
                                     score.goToPosition(120, 0.35);
                                 }
-
                             }else{
                                 if(discontinue.get()){
                                     score.setClawPosition(0.25);
@@ -152,21 +136,18 @@ public class SecondTeleOp extends LinearOpMode {
                                 }
                             }
                         }
-
                         //2220
-
                         pause.set(false);
                     }else if(gamepad1.right_trigger < 0.1){
                         pause.set(true);
                     }
-
+                    score.grabCone();
                 }
             }
         };
-
         waitForStart();
         liftThread.start();
-        int stackHeight = 320;
+        int stackHeight = 300;
         while(opModeIsActive()){
             double gamepadX = gamepad1.left_stick_x;
             double gamepadY = gamepad1.left_stick_y;
@@ -188,11 +169,10 @@ public class SecondTeleOp extends LinearOpMode {
                     drive.setPower(new Vector2D(gamepadX * NORMAL_LINEAR_MODIFIER, gamepadY * NORMAL_LINEAR_MODIFIER), gamepad1.right_stick_x * NORMAL_ROTATIONAL_MODIFIER, false);
                 }
             }
-
             if (gamepad1.dpad_left) {
                 //turn test
                 //drive.turnToInitialPosition();
-                drive.turn(1.57, 0.5);
+                drive.turn(Math.PI/4, 0.3);
             }
             if(gamepad1.a){
                 discontinue.set(true);
@@ -211,7 +191,7 @@ public class SecondTeleOp extends LinearOpMode {
             }
             if(gamepad1.x){
                 //low position
-               score.goToPosition(1120, 1);
+                score.goToPosition(1120, 1);
             }
             if(gamepad1.options){
                 score.resetLiftEncoder();
@@ -219,7 +199,6 @@ public class SecondTeleOp extends LinearOpMode {
             if (gamepad1.share) {
                 drive.resetEncoders();
             }
-
             //TODO: add telemtry for gamepad a and y positions when you press them
             telemetry.addData("flPos", drive.getFLPosition());
             telemetry.addData("frPos", drive.getFRPosition());
@@ -230,15 +209,12 @@ public class SecondTeleOp extends LinearOpMode {
             telemetry.addData("clawPos", score.getClawPosition());
             telemetry.addData("liftPow", score.getPower());
             telemetry.addData("current angle", imu.getAngularOrientation().firstAngle);
-            telemetry.addData("blue", color.currentBlueColor());
-            telemetry.addData("red", color.currentRedColor());
+            //// telemetry.addData("blue", color.currentBlueColor());
+            //telemetry.addData("red", color.currentRedColor());
+            //  telemetry.update();
+            //telemetry.update();
             telemetry.update();
-            telemetry.update();
-            telemetry.update();
-
-
         }
-
         drive.setPower(0, 0, 0, 0);
         score.setPower(0);
         score.setClawPosition(1);
@@ -249,7 +225,5 @@ public class SecondTeleOp extends LinearOpMode {
             int aimedPow = (int) (Math.sqrt(tics) / 15);
             score.setPower(aimedPow);
         }
-
     }
-
 }
