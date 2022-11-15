37d69f06afc4c9706bf65762fc86424c0b8b030a
Experance <100961120+Experance@users.noreply.github.com>
2022-11-14 17:11:49 -0800

    more accurate camera distance to pole updates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/ContourMultiScore.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/ContourMultiScore.java
@@ -28,7 +28,7 @@ public class ContourMultiScore extends OpenCvPipeline {
     private  int cY;
     private List<MatOfPoint> contours = new ArrayList<>();
     private Point[] mainContour;
-    double knownWidth, focalLength, perWidth, distance;
+    double knownWidth, focalLength, perWidth, distance, leftOfContour, rightOfContour = 0;
     Telemetry telemetry;
 
     /* make stuff public static  */
@@ -115,6 +115,16 @@ public class ContourMultiScore extends OpenCvPipeline {
     */
             // gets points of the main contour
             mainContour = contours.get(indexOfMax).toArray();
+            // loops through the x values for the contour
+            for (int i = 0; i < mainContour.length / 2; i++) {
+                leftOfContour += mainContour[i].x;
+            }
+            for (int i = mainContour.length; i > mainContour.length / 2; i--) {
+                rightOfContour += mainContour[i].x;
+            }
+            //gets the average distance between each x point (that's why it's divided by one half of
+            //the values it looped for; the difference means every 2 instead of the average of every 1)
+            perWidth = Math.abs(leftOfContour - rightOfContour) / (mainContour.length / 2);
 
             //draws circle of centroid
             Imgproc.circle(contourMat, new Point(cX, cY), 4, new Scalar(255,49,0,255), 4);
