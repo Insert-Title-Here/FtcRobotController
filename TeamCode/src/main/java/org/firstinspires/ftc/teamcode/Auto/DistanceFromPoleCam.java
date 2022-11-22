3f1d07adfa03b05b6fa04692fc2c6a0741e2d1de
Experance <100961120+Experance@users.noreply.github.com>
2022-11-21 17:32:56 -0800

    cam with contours auto work (back to regular perWidth method)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/DistanceFromPoleCam.java
@@ -0,0 +1,53 @@
+package org.firstinspires.ftc.teamcode.Auto;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvWebcam;
+
+@Autonomous
+public class DistanceFromPoleCam extends LinearOpMode {
+    ContourMultiScore detect;
+    OpenCvWebcam webcam;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        detect = new ContourMultiScore(telemetry);
+
+
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        webcam.setPipeline(detect);
+
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+
+            @Override
+            public void onOpened() {
+                webcam.startStreaming(320, 176, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+                telemetry.addData("Camera Init Error", errorCode);
+                telemetry.update();
+            }
+        });
+
+
+        // ftc dashboard
+        FtcDashboard.getInstance().startCameraStream(webcam, 0);
+        telemetry.addData("Status", "Initialized");
+
+        telemetry.update();
+
+
+        waitForStart();
+        webcam.stopStreaming();
+
+    }
+}
