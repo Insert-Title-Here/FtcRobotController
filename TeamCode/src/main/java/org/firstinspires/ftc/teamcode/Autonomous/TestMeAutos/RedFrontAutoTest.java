3c660c8021e79835eff95196fd4e985c91ff8100
Neha Modi <nehakm2025@gmail.com>
2023-12-01 19:17:17 -0800

    Half of Center Front Red?
    Half of a sketch guestimated auto should be written for front red

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/TestMeAutos/RedFrontAutoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/TestMeAutos/RedFrontAutoTest.java
@@ -116,13 +116,16 @@ public class RedFrontAutoTest extends LinearOpMode {
     }
 
     public void random2() {
-        drive.simpleMoveToPosition(1200, Constants.AUTO_LINEAR_SPEED);
-        drive.simpleMoveToPosition(-100, MecDriveV2.MovementType.STRAFE, Constants.AUTO_LINEAR_SPEED);
-
+        drive.simpleMoveToPosition(100, MecDriveV2.MovementType.STRAFE,Constants.AUTO_LINEAR_SPEED);
+        sleep(500);
+        drive.simpleMoveToPosition(-700, Constants.AUTO_LINEAR_SPEED);
         spikeScore();
-        drive.simpleMoveToPosition(100, Constants.AUTO_LINEAR_SPEED);
+        drive.simpleMoveToPosition(-500, MecDriveV2.MovementType.STRAFE, Constants.AUTO_LINEAR_SPEED);
+        drive.simpleMoveToPosition(200, Constants.AUTO_LINEAR_SPEED);
         drive.tankRotate(-(3.12/2), Constants.AUTO_ROTATIONAL_SPEED);
-        drive.simpleMoveToPosition(-1900, Constants.AUTO_LINEAR_SPEED);
+
+
+        /*drive.simpleMoveToPosition(-1900, Constants.AUTO_LINEAR_SPEED);
         drive.simpleMoveToPosition(-500, MecDriveV2.MovementType.STRAFE, Constants.AUTO_LINEAR_SPEED);
         normalizeStrafe(0, 0.3, 5);
         drive.simpleMoveToPosition(-100, Constants.AUTO_LINEAR_SPEED);
@@ -131,7 +134,7 @@ public class RedFrontAutoTest extends LinearOpMode {
         drive.simpleMoveToPosition(100, Constants.AUTO_LINEAR_SPEED);
         sleep(500);
         drive.simpleMoveToPosition(700, MecDriveV2.MovementType.STRAFE, Constants.AUTO_LINEAR_SPEED);
-        drive.simpleMoveToPosition(-200, Constants.AUTO_LINEAR_SPEED);
+        drive.simpleMoveToPosition(-200, Constants.AUTO_LINEAR_SPEED);*/
 
     }
 
@@ -161,13 +164,14 @@ public class RedFrontAutoTest extends LinearOpMode {
     }
 
     public void spikeScore() {
-        drive.simpleMoveToPosition(-50, Constants.AUTO_SAFE_MO);
+        drive.simpleMoveToPositionTimeout(-200, Constants.AUTO_SAFE_MO, 2);
+        drive.simpleMoveToPosition(-100, Constants.AUTO_SLOWED_SPEED);
         score.setBumperPixelRelease(Constants.AUTO_SCORING_CLAMP_OPEN);
-        drive.simpleMoveToPosition(100, Constants.AUTO_SLOWED_SPEED);
+        drive.simpleMoveToPosition(-100, Constants.AUTO_SLOWED_SPEED);
+        sleep(500);
         score.setBumperPixelRelease(Constants.AUTO_SCORING_CLAMP_CLOSED);
-        drive.simpleMoveToPosition(100, Constants.AUTO_SAFE_MO);
-
     }
+
     public void creep() {
         drive.setPowerAuto(Constants.AUTO_SLOWED_SPEED, MecDriveV2.MovementType.STRAIGHT);
         //Change me!
@@ -176,16 +180,19 @@ public class RedFrontAutoTest extends LinearOpMode {
     }
 
     public void autoScore() {
+        score.setGrabberPosition(Constants.GRABBING);
+        score.goToLiftTarget(100, 0.8);
+        sleep(500);
         score.setLinkagePositionLogistic(0.5, 1000, 100);
         //  score.goToLiftTarget(Constants.LIFT_LOW, 0.8);
         score.setLinkagePositionLogistic(Constants.LINKAGE_UP, 1500, 100);
         sleep(300);
         score.setGrabberPosition(Constants.OPEN);
         sleep(300);
+        drive.simpleMoveToPosition(100, Constants.AUTO_SAFE_MO);
         score.setLinkagePositionLogistic(0.5, 1000, 100);
         //   score.goToLiftTarget(0, 0.3);
         score.setLinkagePositionLogistic(Constants.LINKAGE_DOWN, 1000, 100);
-
     }
 
     public void initAprilTags() {
